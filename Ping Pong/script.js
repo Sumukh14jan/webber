039542dddb51f8abb52c3adca841712349f9710f
Sumukh14jan <sumukh14jan@gmail.com>
2021-06-10 18:46:56 +0530

    Ping pong made successfully

--- Ping Pong/script.js
+++ Ping Pong/script.js
@@ -3,19 +3,32 @@ const rod1 = document.getElementById('rod1');
 const rod2 = document.getElementById('rod2');
 const r = document.querySelector(':root');
 
+
 function applycol() {
     r.style.setProperty('--rod1col', document.querySelector('#rod1col').value);
     r.style.setProperty('--rod2col', document.querySelector('#rod2col').value);
     r.style.setProperty('--ballcol', document.querySelector('#ballcol').value);
+    if(document.querySelector("#flexRadioDefault1").checked){
+        document.querySelector("#control").innerHTML = "                    <div class=\"modal-body\" id=\"control\">\n" +
+            "                        <div class=\"padded\"><kbd>Right arrow(<span class=\"arrow\">➡</span>) or A</kbd>: Move player 1's rod to right.<br></div>\n" +
+            "                        <div class=\"padded\"><kbd>Left arrow(<span class=\"arrow\">⬅</span>) or D</kbd>: Move player 1's rod to left.</div>"
+    }else{
+        document.querySelector("#control").innerHTML = "<div class=\"padded\"><kbd>Right arrow(<span class=\"arrow\">➡</span>) or A</kbd>: Move player 1's rod to right.<br></div>\n" +
+            "<div class=\"padded\"><kbd>D</kbd>: Move player 2's rod to right.</div>\n" +
+            "\n" +
+            "<div class=\"padded\"><kbd>Left arrow(<span class=\"arrow\">⬅</span>)</kbd>: Move player 1's rod to left.</div>\n" +
+            "<div class=\"padded\"><kbd>A</kbd>: Move player 2's rod to left.</div>"
+    }
 }
 
 const storeName = "PPName";
 const storeScore = "PPMaxScore";
-let rod1Name = "Rod 1";
-let rod2Name = "Rod 2";
+let rod1Name = "Player1";
+let rod2Name = "Player2";
 
 
 let score,
+    score1=0,
     maxScore,
     movement,
     rod,
@@ -30,14 +43,8 @@ let windowWidth = window.innerWidth,
 
 
 (function () {
-    rod = localStorage.getItem(storeName);
-    maxScore = localStorage.getItem(storeScore);
-
-    if (rod === "null" || maxScore === "null") {
-        maxScore = 0;
-        rod = "Rod1"
-    } else {
-    }
+    maxScore = 0;
+    rod = "Rod1";
 
     resetBoard(rod);
 })();
@@ -61,6 +68,7 @@ function resetBoard(rodName) {
     }
 
     score = 0;
+    score1=0;
     gameOn = false;
 
 }
@@ -69,33 +77,44 @@ function resetBoard(rodName) {
 
 function storeWin(rod, score) {
 
-    if (score > maxScore) {
-        maxScore = score;
-        localStorage.setItem(storeName, rod);
-        localStorage.setItem(storeScore, maxScore);
-    }
 
     clearInterval(movement);
     resetBoard(rod);
-
-    alert(rod + " wins with a score of " + (score * 100) + ". Max score is: " + (maxScore * 100));
+    if(rod=="player1"){alert(rod + " wins with a score of " + (score1 * 100));}else{alert(rod + " wins with a score of " + (score * 100));}
 
 }
 
 
 
-window.addEventListener('keypress', function () {
+
+
+
+window.addEventListener('keydown', function () {
+    console.log(event.code);
     let rodSpeed = 20;
 
     let rodRect = rod1.getBoundingClientRect();
+    let rodRect2 = rod2.getBoundingClientRect();
 
+    if (document.querySelector("#flexRadioDefault1").checked){
+        if (event.code === "KeyD" && ((rodRect.x + rodRect.width) < window.innerWidth)) {
+            rod1.style.left = (rodRect.x) + rodSpeed + 'px';
+            rod2.style.left = rod1.style.left;
+        } else if (event.code === "KeyA" && (rodRect.x > 0)) {
+            rod1.style.left = (rodRect.x) - rodSpeed + 'px';
+            rod2.style.left = rod1.style.left;
+    }}else{
+        if (event.code === "KeyD" && ((rodRect.x + rodRect.width) < window.innerWidth)) {
+            rod1.style.left = (rodRect.x) + rodSpeed + 'px';
+        } else if (event.code === "KeyA" && (rodRect.x > 0)) {
+            rod1.style.left = (rodRect.x) - rodSpeed + 'px';
+        }
+        if (event.code === "ArrowRight" && ((rodRect2.x + rodRect2.width) < window.innerWidth)) {
+            rod2.style.left = (rodRect2.x) + rodSpeed + 'px';
+        } else if (event.code === "ArrowLeft" && (rodRect2.x > 0)) {
+            rod2.style.left = (rodRect2.x) - rodSpeed + 'px';
+        }
 
-    if (event.code === "KeyD" && ((rodRect.x + rodRect.width) < window.innerWidth)) {
-        rod1.style.left = (rodRect.x) + rodSpeed + 'px';
-        rod2.style.left = rod1.style.left;
-    } else if (event.code === "KeyA" && (rodRect.x > 0)) {
-        rod1.style.left = (rodRect.x) - rodSpeed + 'px';
-        rod2.style.left = rod1.style.left;
     }
 
 
@@ -103,8 +122,6 @@ window.addEventListener('keypress', function () {
 
         if (!gameOn) {
             gameOn = true;
-            rod1Name = prompt("Enter player 1 name");
-            rod2Name = prompt("Enter player 2 name");
             let ballRect = ball.getBoundingClientRect();
             let ballX = ballRect.x;
             let ballY = ballRect.y;
@@ -117,12 +134,12 @@ window.addEventListener('keypress', function () {
 
 
             movement = setInterval(function () {
-                // Move ball 
+                // Move ball
                 ballX += ballSpeedX;
                 ballY += ballSpeedY;
 
-                rod1X = rod1.getBoundingClientRect().x;
-                rod2X = rod2.getBoundingClientRect().x;
+                let rod1X = rod1.getBoundingClientRect().x;
+                let rod2X = rod2.getBoundingClientRect().x;
 
                 ball.style.left = ballX + 'px';
                 ball.style.top = ballY + 'px';
@@ -136,9 +153,10 @@ window.addEventListener('keypress', function () {
                 let ballPos = ballX + ballDia / 2;
 
                 // Check for Rod 1
-                if (ballY <= rod1Height) {
+                if (ballY <= 75+rod1Height) {
                     ballSpeedY = -ballSpeedY; // Reverses the direction
-                    score++;
+                    document.querySelector(".rod1").innerText = `Score: ${score1*100}`;
+                    score1++;
 
                     // Check if the game ends
                     if ((ballPos < rod1X) || (ballPos > (rod1X + rod1Width))) {
@@ -149,6 +167,7 @@ window.addEventListener('keypress', function () {
                 // Check for Rod 2
                 else if ((ballY + ballDia) >= (windowHeight - rod2Height)) {
                     ballSpeedY = -ballSpeedY; // Reverses the direction
+                    document.querySelector(".rod2").innerText = `Score: ${score*100}`;
                     score++;
 
                     // Check if the game ends
